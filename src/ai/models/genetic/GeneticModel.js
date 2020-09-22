19588c333d51137c56def344a73ca41775d36402
jovanl <jovan.lazic@plusplus.rs>
2020-09-22 13:08:29 +0200

    izmenjen model

--- src/ai/models/genetic/GeneticModel.js
+++ src/ai/models/genetic/GeneticModel.js
@@ -31,7 +31,7 @@ export default class GeneticModel extends Model {
     request.onload = function () {
       const data = JSON.parse(this.response)
       // Begin accessing JSON data here
-    console.log('mutate:stigli podaci:',data)
+    console.log('cross:stigli podaci:',data)
     chromosomes = data;
     }
 
@@ -40,26 +40,6 @@ export default class GeneticModel extends Model {
       chromo:chromosomes
     }))
 
-    const offspring1 = parents[0];
-    const offspring2 = parents[1];
-    console.info("off1:",offspring1);
-    console.info("off2:",offspring2);
-    // Select a random crossover point
-    const crossOverPoint = Math.floor(Math.random() * offspring1.length);
-    console.info("cross here: ",crossOverPoint);
-    // Swap values among parents
-    for (let i = 0; i < crossOverPoint; i += 1) {
-        const temp = offspring1[i];
-        offspring1[i] = offspring2[i];
-        offspring2[i] = temp;
-    }
-    const offspring = [offspring1, offspring2];
-    // Replace the last 2 with the new offspring
-    for (let i = 0; i < 2; i += 1) {
-      chromosomes[chromosomes.length - i - 1] = offspring[i];
-    }
-    console.info("child: ",offspring)
-    return offspring;
   }
 
   mutate(chromosomes) {
